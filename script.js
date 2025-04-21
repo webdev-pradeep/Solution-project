75ab5246a9eca2fa9e3ad45ae99a00acd18e83d6
Pradeep <webdevpradeep0101@gmail.com>
2025-04-21 05:55:49 -1200

    folder name chande

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ buttons.forEach((button) => {
     // console.log(event.target);
     if (event.target.id === "white") {
       body.style.background = "white";
+      body.style.color = "grey";
     }
     if (event.target.id === "grey") {
       body.style.background = "grey";
+      body.style.color = "white";
     }
     if (event.target.id === "yellow") {
       body.style.background = "yellow";
+      body.style.color = "blue";
     }
     if (event.target.id === "blue") {
       body.style.background = "blue";
+      body.style.color = "yellow";
     }
   });
 });
